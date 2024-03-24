7729b115b569a136087e1c7b9286742355554ba3
Smeet-Dedhia <smeetd1502@gmail.com>
2024-03-24 09:18:32 +0530

    Fix image location

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -12,7 +12,7 @@ export class HousingService {
       "name": "Acme Fresh Start Housing",
       "city": "Chicago",
       "state": "IL",
-      "photo": "/assets/bernard-hermant-CLKGGwIBTaY-unsplash.jpg",
+      "photo": "assets/bernard-hermant-CLKGGwIBTaY-unsplash.jpg",
       "availableUnits": 4,
       "wifi": true,
       "laundry": true
@@ -22,7 +22,7 @@ export class HousingService {
       "name": "A113 Transitional Housing",
       "city": "Santa Monica",
       "state": "CA",
-      "photo": "/assets/brandon-griggs-wR11KBaB86U-unsplash.jpg",
+      "photo": "assets/brandon-griggs-wR11KBaB86U-unsplash.jpg",
       "availableUnits": 0,
       "wifi": false,
       "laundry": true
@@ -32,7 +32,7 @@ export class HousingService {
       "name": "Warm Beds Housing Support",
       "city": "Juneau",
       "state": "AK",
-      "photo": "/assets/i-do-nothing-but-love-lAyXdl1-Wmc-unsplash.jpg",
+      "photo": "assets/i-do-nothing-but-love-lAyXdl1-Wmc-unsplash.jpg",
       "availableUnits": 1,
       "wifi": false,
       "laundry": false
@@ -42,7 +42,7 @@ export class HousingService {
       "name": "Homesteady Housing",
       "city": "Chicago",
       "state": "IL",
-      "photo": "/assets/ian-macdonald-W8z6aiwfi1E-unsplash.jpg",
+      "photo": "assets/ian-macdonald-W8z6aiwfi1E-unsplash.jpg",
       "availableUnits": 1,
       "wifi": true,
       "laundry": false
@@ -52,7 +52,7 @@ export class HousingService {
       "name": "Happy Homes Group",
       "city": "Gary",
       "state": "IN",
-      "photo": "/assets/krzysztof-hepner-978RAXoXnH4-unsplash.jpg",
+      "photo": "assets/krzysztof-hepner-978RAXoXnH4-unsplash.jpg",
       "availableUnits": 1,
       "wifi": true,
       "laundry": false
@@ -62,7 +62,7 @@ export class HousingService {
       "name": "Hopeful Apartment Group",
       "city": "Oakland",
       "state": "CA",
-      "photo": "/assets/r-architecture-JvQ0Q5IkeMM-unsplash.jpg",
+      "photo": "assets/r-architecture-JvQ0Q5IkeMM-unsplash.jpg",
       "availableUnits": 2,
       "wifi": true,
       "laundry": true
@@ -72,7 +72,7 @@ export class HousingService {
       "name": "Seriously Safe Towns",
       "city": "Oakland",
       "state": "CA",
-      "photo": "/assets/phil-hearing-IYfp2Ixe9nM-unsplash.jpg",
+      "photo": "assets/phil-hearing-IYfp2Ixe9nM-unsplash.jpg",
       "availableUnits": 5,
       "wifi": true,
       "laundry": true
@@ -82,7 +82,7 @@ export class HousingService {
       "name": "Hopeful Housing Solutions",
       "city": "Oakland",
       "state": "CA",
-      "photo": "/assets/r-architecture-GGupkreKwxA-unsplash.jpg",
+      "photo": "assets/r-architecture-GGupkreKwxA-unsplash.jpg",
       "availableUnits": 2,
       "wifi": true,
       "laundry": true
@@ -92,7 +92,7 @@ export class HousingService {
       "name": "Seriously Safe Towns",
       "city": "Oakland",
       "state": "CA",
-      "photo": "/assets/saru-robert-9rP3mxf8qWI-unsplash.jpg",
+      "photo": "assets/saru-robert-9rP3mxf8qWI-unsplash.jpg",
       "availableUnits": 10,
       "wifi": false,
       "laundry": false
@@ -102,7 +102,7 @@ export class HousingService {
       "name": "Capital Safe Towns",
       "city": "Portland",
       "state": "OR",
-      "photo": "/assets/webaliser-_TPTXZd9mOo-unsplash.jpg",
+      "photo": "assets/webaliser-_TPTXZd9mOo-unsplash.jpg",
       "availableUnits": 6,
       "wifi": true,
       "laundry": true
